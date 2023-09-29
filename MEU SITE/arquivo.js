6233779e64fc8fd92fb598111df9efdda6b38f26
ricardoOCR <142262057+ricardoOCR@users.noreply.github.com>
2023-09-29 14:06:15 -0300

    Add files via upload

---
+++ MEU SITE/arquivo.js
@@ -0,0 +1,13 @@
+
+// Captura o botão pelo ID
+const meuBotao = document.getElementById("meuBotao");
+
+
+// Função para ser executada quando o botão for clicado
+function redirecioneParaMinhaPagina() {
+    // Redireciona o usuário para minha página
+    window.location.href = "https://www.bol.uol.com.br/entretenimento/2022/12/05/copa-do-mundo-meme-2014.htm"
+};
+
+// Define o evento de clique no botão
+meuBotao.addEventListener("click", redirecioneParaMinhaPagina());
